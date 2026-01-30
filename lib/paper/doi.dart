5a23e166f5cffcf197b2a22380c920cfea9b2dae
Filippo Valle <filippo_valle@protonmail.com>
2026-01-30 16:56:43 +0100

    fix semantichscholar

--- lib/paper/doi.dart
+++ lib/paper/doi.dart
@@ -4,9 +4,9 @@ import 'package:zotero_app/library/Paper.dart';
 launchURLdoi(Paper paper) async {
   String doi = paper.doi ?? "/";
   var url = Uri.parse("https://doi.org/" + doi);
-  if (await canLaunchUrl(url)) {
-    await launchUrl(url);
-  } else {
-    throw 'Could not launch $url';
+  try {
+    await launchUrl(url, mode: LaunchMode.externalApplication);
+  } catch (e) {
+    throw 'Could not launch $url: $e';
   }
 }
